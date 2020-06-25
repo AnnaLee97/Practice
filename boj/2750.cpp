0e66361605f1397c454854ee30763516d121db5f
SooKyeongLee <bbluv_@naver.com>
2020-06-25 11:55:34 +0900

    Create 2750.cpp

---
+++ boj/2750.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    vector<int> arr(n);
+    
+    for(int i=0;i<n;i++){
+        scanf("%d", &arr[i]);
+    }
+    
+    sort(arr.begin(), arr.end());
+    
+    for(int i=0;i<n;i++){
+        printf("%d\n", arr[i]);
+    }
+    
+    return 0;
+}
