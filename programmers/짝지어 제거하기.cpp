8450b6fc74c524f25fd0793fe9ff53b56a7c1c81
SooKyeongLee <bbluv_@naver.com>
2021-09-06 14:51:06 +0900

    #include <stack> stack<char> st

---
+++ programmers/짝지어 제거하기.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int solution(string s)
+{
+    int answer = 0;
+    stack<char> st;
+    
+    for(int i=0; i<s.length(); i++){
+        if(st.size() != 0 && st.top() == s[i]){
+            st.pop();
+        } else {
+            st.push(s[i]);
+        }
+    }
+    if(st.size()==0) answer = 1;
+
+
+    return answer;
+}
